487442ebb6fc251c468ab165eff23a6e4dfbd50c
Lootmann <lyotsu.programming.or.some@gmail.com>
2023-04-10 06:37:17 +0900

    app/page.tsx: show all articles using prisma client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
-export default function Home() {
+import { getAllArticles } from "@/apis/articles";
+
+export default async function Home() {
+  const articles = await getAllArticles();
+
   return (
     <div>
-      <p className="text-2xl">hello world</p>
+      <h2>Hello Nextjs World :D</h2>
+
+      <div>
+        <ul>
+          {articles.map((article) => (
+            <li key={article.id}>{article.title}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
