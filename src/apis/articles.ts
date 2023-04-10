5d235e02f99637dd77717fa3574e35d18192792e
Lootmann <lyotsu.programming.or.some@gmail.com>
2023-04-10 13:12:22 +0900

    articles/slug: when fail to find an article with slug, show not found page

--- src/apis/articles.ts
+++ src/apis/articles.ts
@@ -3,19 +3,17 @@ import { Article } from "@prisma/client";
 
 export const getAllArticles = async () => {
   const res = await fetch("http://localhost:3000/api/articles");
-  if (!res.ok) throw new Error("hoge");
-
   const data = await res.json();
   return data.articles as Article[];
 };
 
 export const getArticleBySlug = async (slug: string) => {
   const res = await fetch(`http://localhost:3000/api/articles/${slug}`);
-  if (!res.ok) throw new Error("!getArticleBySlug");
+  if (!res.ok) notFound();
+
   const data = await res.json();
   const article = data.article;
 
   if (article === null) notFound();
-
   return article;
 };
