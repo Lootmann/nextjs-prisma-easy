a81982efb6ea5c358423624329e4eea307eb0a48
Lootmann <lyotsu.programming.or.some@gmail.com>
2023-04-10 12:59:40 +0900

    apis/comments: add type hint

--- src/apis/comments.ts
+++ src/apis/comments.ts
@@ -1,3 +1,5 @@
+import { Comment } from "@prisma/client";
+
 export const getCommentsByArticleId = async (articleId: number) => {
   const res = await fetch(`http://localhost:3000/api/comments/${articleId}`);
   const data = await res.json();
